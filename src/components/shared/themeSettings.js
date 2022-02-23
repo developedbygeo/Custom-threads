6e392cf8bbbd996fbff73a40567109028cf17e22
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-23 03:07:22 +0200

    style: adding the base palette

---
+++ src/components/shared/themeSettings.js
@@ -0,0 +1,14 @@
+const theme = {
+  colors: {
+    mainText: '#323334',
+    lightText: '#989999',
+    mainBg: '#fdfdfd',
+    cardBg: '#eaeaea',
+    footerBg: '#222222',
+    ctaLightGray: '#a3a3a3',
+    ctaDavysGray: '#5c5c5c',
+    ctaEerieBlack: '#141414',
+  },
+};
+
+export default theme;
