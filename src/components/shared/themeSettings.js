d884fa6db2b402f60e945dbadc0dca102b78105c
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-24 01:55:49 +0200

    style: updating palette

--- src/components/shared/themeSettings.js
+++ src/components/shared/themeSettings.js
@@ -3,11 +3,15 @@ const theme = {
     mainText: '#323334',
     lightText: '#989999',
     mainBg: '#fdfdfd',
+    heroBg: '#e2d9d6',
+    promoText: 'a86a3d',
     cardBg: '#eaeaea',
     footerBg: '#222222',
     ctaLightGray: '#a3a3a3',
     ctaDavysGray: '#5c5c5c',
     ctaEerieBlack: '#141414',
+    ctaSecondaryBgClr: '#a96248',
+    activeNav: '#dd6636',
   },
 };
 
