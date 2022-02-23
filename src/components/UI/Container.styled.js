51a4f2d62d8ab7e0ae65e7e2e8022ca8cbc38449
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-24 01:53:40 +0200

    style: adding the styled sections

---
+++ src/components/UI/Container.styled.js
@@ -0,0 +1,27 @@
+import styled, { css } from 'styled-components';
+
+import { getLayout } from '../shared/utils';
+
+const sectionDefaults = css`
+  width: 100vw;
+  height: auto;
+`;
+
+export const StyledHero = styled.section`
+  ${sectionDefaults}
+  ${getLayout}
+  background: ${({ bgClr }) => bgClr || 'inherit'};
+  max-height: 45vh;
+
+  button {
+    margin-block: 2rem;
+  }
+  p {
+    font-weight: 300;
+    font-size: 1.8rem;
+    letter-spacing: 0.2rem;
+  }
+  h2 {
+    margin-block: 0.5rem;
+  }
+`;
