5612e75c08b0ea246e61bdeb3e1abd1335d00029
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-24 01:53:10 +0200

    feat: adding Routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './components/shared/globalStyle';
 import theme from './components/shared/themeSettings';
 
 import Header from './components/Header/Header';
+import Landing from './pages/Landing';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Header />
-      <main></main>
+      <main>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+      </main>
     </ThemeProvider>
   );
 }
