07d56641f898aeea849a4fd60d3e291302a84876
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-24 01:55:29 +0200

    refactor: adding getColor and getLayout util fns

---
+++ src/components/shared/utils.js
@@ -0,0 +1,15 @@
+import { flexMixin, gridMixin } from './mixins';
+
+export const getColor =
+  (clr) =>
+  ({ theme }) =>
+    theme.colors[clr];
+
+export const getLayout = ({ flexSettings, gridSettings }) => {
+  if (flexSettings) {
+    return flexMixin(flexSettings.justify, flexSettings.align, flexSettings.dir);
+  }
+  if (gridSettings) {
+    return gridMixin(gridSettings.cols, gridSettings.rows);
+  }
+};
