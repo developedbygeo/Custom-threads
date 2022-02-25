5ad5d8e946e47784b92475ee3411ae212a69cb7b
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-25 22:27:03 +0200

    chore: adding the dispatch call to the thunk

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 
+import { fetchDataAPI } from './features/dataApi';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './components/shared/globalStyle';
 import theme from './components/shared/themeSettings';
@@ -8,7 +11,13 @@ import Header from './components/Header/Header';
 import Landing from './pages/Landing';
 import Home from './pages/Home';
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchDataAPI());
+  }, [dispatch]);
+
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
@@ -21,6 +30,6 @@ function App() {
       </main>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
