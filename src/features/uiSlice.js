2c13dd93cb5b84b8bf59b4ca993c61a32e3c8b9c
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-25 21:51:22 +0200

    chore: creating the ui state slice

---
+++ src/features/uiSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const uiSlice = createSlice({
+  name: 'ui',
+  initialState: { notification: null },
+  reducers: {
+    createNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
+});
+export const uiActions = uiSlice.actions;
+export default uiSlice;
