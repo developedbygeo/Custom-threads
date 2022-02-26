6aee34b41be5ab258d9764086374d74b1c8b7624
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-26 04:20:12 +0200

    style: making header responsive and adding styling to the navs

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
-import { flexMixin } from '../shared/mixins';
+
+import { flexMixin, gridMixin, colorInteract, showActive } from '../shared/mixins';
+import { devices } from '../shared/breakpoints';
 
 const StyledHeader = styled.header`
   width: 100vw;
   height: 10vh;
   position: relative;
-  padding: 2rem;
+  padding: 0 2rem;
   ${flexMixin('space-evenly', 'center', 'row')};
   & > a {
     flex: 2;
@@ -28,23 +30,56 @@ const StyledHeader = styled.header`
     outline: none;
     background: transparent;
     min-width: 5.5rem;
-    min-height: 5.5rem;
+    min-height: 7.5rem;
     font-size: 1.8rem;
     transition: all 150ms ease-in-out;
     ${flexMixin('center', 'center', 'row')}
-    color: ${({ theme }) => theme.colors.mainText};
-    &:hover {
-      color: ${({ theme }) => theme.colors.activeNav};
-    }
   }
-  svg {
+  button,
+  a:not(.logo) {
+    ${colorInteract()}
+  }
+  .active {
+    ${showActive()}
+  }
+  svg,
+  span {
+    width: 100%;
+    height: 100%;
     min-width: 2rem;
     min-height: 2rem;
-    max-width: 65%;
+    max-width: 4rem;
     max-height: 80%;
   }
   .bag {
-    max-width: 60%;
+    max-width: 4.5rem;
+  }
+  .total {
+    margin-left: 1rem;
+  }
+
+  @media screen and (max-width: 900px) {
+    .desktopNav {
+      display: none;
+    }
+  }
+  @media screen and (min-width: 900px) {
+    .mobileNav {
+      display: none;
+    }
+    .desktopNav {
+      ${flexMixin('space-between', 'center', 'row')};
+      flex: 3.5;
+      & > div {
+        ${flexMixin('space-evenly', 'center', 'row')};
+        gap: 2rem;
+        &:not(.desktopNavCtrl) {
+          height: 8vh;
+          border: 2px solid ${({ theme }) => theme.colors.heroBg};
+          border-inline: none;
+        }
+      }
+    }
   }
 `;
 
