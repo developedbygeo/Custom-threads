5686f4d30a8d043878016832e2bc9e506d094840
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-26 04:19:25 +0200

    adding custom styling and a helper fn for the Card

--- src/components/UI/Card.styled.js
+++ src/components/UI/Card.styled.js
@@ -1,12 +1,22 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { devices } from '../shared/breakpoints';
 import { getLayout } from '../shared/utils';
+
+const baseCardSettings = css`
+  width: ${({ w }) => w || 'initial'};
+`;
 
 export const Card = styled.div`
   padding: ${({ p }) => p || '2rem'};
   background: ${({ theme, BgClr }) => BgClr || theme.colors.cardBgClr};
-  margin: ${({ customMargin }) => customMargin || 'initial'};
+  margin: ${({ customMargin }) => customMargin || 'auto'};
+  text-align: ${({ textAlign }) => textAlign || 'initial'};
   ${getLayout}
+  ${baseCardSettings}
+
+  &>div {
+    width: ${({ w }) => w || '100%'};
+  }
 
   p {
     margin-block: 1rem;
@@ -19,6 +29,8 @@ export const ImageCard = styled.div`
   max-width: fit-content;
   max-height: fit-content;
   align-self: ${({ alignSelf }) => alignSelf || 'initial'};
+  justify-self: ${({ justifySelf }) => justifySelf || 'initial'};
+  text-align: ${({ textAlign }) => textAlign || 'initial'};
   & > img {
     max-width: 30rem;
     height: 100%;
