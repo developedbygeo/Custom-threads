e96bcd091be96492f7a3e917a4d06e9250887327
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-27 02:05:42 +0200

    fix: fixing incorrect hex

--- src/components/shared/themeSettings.js
+++ src/components/shared/themeSettings.js
@@ -4,7 +4,7 @@ const theme = {
     lightText: '#989999',
     mainBg: '#fdfdfd',
     heroBg: '#e2d9d6',
-    promoText: 'a86a3d',
+    promoText: '#a86a3d',
     cardBg: '#eaeaea',
     footerBg: '#222222',
     ctaLightGray: '#a3a3a3',
