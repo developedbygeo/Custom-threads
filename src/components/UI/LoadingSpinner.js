adaa047117d4b66723b013ab435ac30d4610cb86
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-27 02:03:54 +0200

    style: updating Spinner's colors/position and adding a message

--- src/components/UI/LoadingSpinner.js
+++ src/components/UI/LoadingSpinner.js
@@ -1,10 +1,12 @@
 import styled from 'styled-components/macro';
-import { spinning } from '../animations';
+import { spinning } from '../shared/animations';
 
 const Spinner = styled.div`
   display: inline-block;
   width: 55px;
   height: 55px;
+  align-self: ${({ alignSelf }) => alignSelf || 'initial'};
+  justify-self: ${({ justifySelf }) => justifySelf || 'initial'};
   &:after {
     content: ' ';
     display: block;
@@ -12,16 +14,25 @@ const Spinner = styled.div`
     height: 40px;
     margin: 0.4rem;
     border-radius: 50%;
-    border: 6px solid ${({ theme }) => theme.colors.spinnerClr};
-    border-color: ${({ theme }) => theme.colors.spinnerClr} transparent
-      ${({ theme }) => theme.colors.spinnerClr} transparent;
+    border: 6px solid ${({ theme }) => theme.colors.promoText};
+    border-color: ${({ theme }) => theme.colors.promoText} transparent
+      ${({ theme }) => theme.colors.promoText} transparent;
     animation: ${spinning} 2s linear infinite;
   }
+`;
+const SpinnerMessage = styled.p`
+  text-align: center;
+  letter-spacing: 0.11rem;
 `;
 Spinner.displayName = 'Spinner';
 
 const LoadingSpinner = () => {
-  return <Spinner></Spinner>;
+  return (
+    <>
+      <Spinner></Spinner>
+      <SpinnerMessage>Please try refreshing your browser</SpinnerMessage>
+    </>
+  );
 };
 
 export default LoadingSpinner;
