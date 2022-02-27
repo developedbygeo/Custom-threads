1f1e308825fcbd3087e93478a3a3ccfae8e16ec2
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-28 00:23:07 +0200

    refactor: making controls more modular

--- src/components/Extra/Controls.js
+++ src/components/Extra/Controls.js
@@ -1,15 +1,30 @@
+import { useState } from 'react';
+
+import Drawer from '../UI/Drawer/Drawer';
+import Filters from './Filters';
 import { StyledUtilityBtn } from '../UI/Button.styled';
 import { Card } from '../UI/Card.styled';
 
-// TODO modularize Controls
+export const UtilityButton = ({ children }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export const UtilityButton = ({ title, children }) => {
+  const menuToggleHandler = () => {
+    setMenuOpen((prevState) => !prevState);
+  };
+
   return (
-    <Card p="1rem">
-      <StyledUtilityBtn title={title}>
-        <span className="icon">{children[0]}</span>
-        <span className="text">{children[1]}</span>
-      </StyledUtilityBtn>
-    </Card>
+    <>
+      <Card sticky p="1rem">
+        <StyledUtilityBtn onClick={menuToggleHandler}>
+          <span className="icon">{children[0]}</span>
+          <span className="text">{children[1]}</span>
+        </StyledUtilityBtn>
+      </Card>
+      {menuOpen && (
+        <Drawer>
+          <Filters />
+        </Drawer>
+      )}
+    </>
   );
 };
