1da1fdc750f770c13de511e2b8f4a23b8b3a5ca2
georgiosan <georgiosanagnostou@hotmail.com>
2022-02-27 02:02:03 +0200

    refactor: consolidating the middleware

--- src/features/dataApi.js
+++ src/features/dataApi.js
@@ -3,30 +3,31 @@ import { uiActions } from './uiSlice';
 
 export const fetchDataAPI = () => {
   return async (dispatch) => {
-    const fetchData = async () => {
-      let data;
-      if (!localStorage.getItem('ecommerceDevGeoData')) {
+    if (localStorage.getItem('ecommerceDevGeoData') !== null) {
+      const dataLS = JSON.parse(localStorage.getItem('ecommerceDevGeoData'));
+      dispatch(productActions.addProducts(dataLS));
+      return dataLS;
+    } else {
+      const fetchData = async () => {
         const res = await fetch('https://fakestoreapi.com/products');
         if (!res.ok) {
           throw new Error('Failed to retrieve data');
         }
-        data = await res.json();
+        const data = await res.json();
         localStorage.setItem('ecommerceDevGeoData', JSON.stringify(data));
-      } else {
-        const dataLS = localStorage.getItem('ecommerceDevGeoData');
-        data = JSON.parse(dataLS);
+        return data;
+      };
+      try {
+        const dataAPI = await fetchData();
+        console.log('async');
+        dispatch(productActions.addProducts(dataAPI));
+      } catch (err) {
+        uiActions.createNotification({
+          status: 'error',
+          title: 'Error!',
+          message: err.message,
+        });
       }
-      return data;
-    };
-    try {
-      const dataAPI = await fetchData();
-      dispatch(productActions.addProducts(dataAPI));
-    } catch (err) {
-      uiActions.createNotification({
-        status: 'error',
-        title: 'Error!',
-        message: err.message,
-      });
     }
   };
 };
