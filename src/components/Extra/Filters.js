701bb814e6af42ad52b10732939269dadc0f54aa
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-01 04:48:19 +0200

    feat: adding a responsive, styled slider & submit button

--- src/components/Extra/Filters.js
+++ src/components/Extra/Filters.js
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 import StyledFilters from './Filters.styled';
+import { CtaButton } from '../UI/Button.styled';
 import Checkbox from '../UI/Checkbox/Checkbox';
+import CustomSlider from '../UI/Slider.styled';
 
 const Filters = () => {
+  const [price, setPrice] = useState(1000);
+
+  const changePriceHandler = (e) => {
+    setPrice(e.target.value);
+  };
+
   return (
     <>
       <StyledFilters>
@@ -11,9 +21,21 @@ const Filters = () => {
         <Checkbox>Jewelry</Checkbox>
         <Checkbox>Electronics</Checkbox>
         <h3>By Price</h3>
-        <label>Price</label>
-        <input type="range" min="20" max="1000" defaultValue="0" />
-        <button>Search</button>
+        <div className="price">
+          <label>Price:</label>
+          <p className="selected-price">$ {price}</p>
+        </div>
+        <CustomSlider
+          onChange={changePriceHandler}
+          type="range"
+          min="100"
+          max="1000"
+          step="100"
+          value={price}
+        />
+        <CtaButton p="1.75rem 6rem" alignSelf="center">
+          Search
+        </CtaButton>
       </StyledFilters>
     </>
   );
