9830c8d2e4c756d5db19e586c83793c1dd1c1e95
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-01 04:52:08 +0200

    fix: fixing the incorrect alpha for the hover state

--- src/components/UI/Checkbox/Checkbox.styled.js
+++ src/components/UI/Checkbox/Checkbox.styled.js
@@ -5,10 +5,7 @@ import { flexMixin } from '../../shared/mixins';
 const StyledCheckbox = styled.div`
   padding: ${({ p }) => p || '1.25rem'} ${flexMixin('center', 'flex-start', 'column')};
   gap: ${({ gap }) => gap || '2rem'};
-  label {
-    font-weight: 200;
-    font-size: 2rem;
-  }
+
   input[type='checkbox'] {
     clip: rect(0 0 0 0);
     clip-path: inset(50%);
@@ -26,16 +23,16 @@ const StyledCheckbox = styled.div`
     width: 3rem;
     border: 1px solid ${({ theme }) => theme.colors.davysGrayCta};
     background: ${({ theme }) => theme.colors.mainBg};
-    transition: all 100ms ease-in-out;
+    transition: all 50ms linear;
   }
   @media (hover: hover) {
     .checkbox:hover {
-      background: rgba(${({ theme }) => theme.colors.activeNavRGB}, 0.15);
+      background: rgba(${({ theme }) => theme.colors.activeNavRGB}, 0.75);
       border: none;
     }
   }
   .checkbox--active {
-    background: rgba(${({ theme }) => theme.colors.activeNavRGB}, 0.75);
+    background: rgba(${({ theme }) => theme.colors.activeNavRGB}, 0.95);
     svg {
       color: ${({ theme }) => theme.colors.mainBg};
     }
