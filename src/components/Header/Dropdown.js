8104e6ed9e544b1b0ee53123483844e2705ef5f3
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-04 02:42:35 +0200

    refactor: code cleanup - removing the onClick handlers

--- src/components/Header/Dropdown.js
+++ src/components/Header/Dropdown.js
@@ -2,47 +2,22 @@ import { NavLink } from 'react-router-dom';
 
 import StyledDropdown from './Dropdown.styled';
 
-const Dropdown = ({ onActivate }) => {
+const Dropdown = () => {
   return (
     <StyledDropdown>
-      <NavLink
-        onClick={onActivate}
-        title="Men"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-        to="/home"
-      >
+      <NavLink title="Men" className={({ isActive }) => (isActive ? 'active' : '')} to="/home">
         Home
       </NavLink>
-      <NavLink
-        onClick={onActivate}
-        title="Men"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-        to="/men"
-      >
+      <NavLink title="Men" className={({ isActive }) => (isActive ? 'active' : '')} to="/men">
         Men
       </NavLink>
-      <NavLink
-        onClick={onActivate}
-        title="Women"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-        to="/women"
-      >
+      <NavLink title="Women" className={({ isActive }) => (isActive ? 'active' : '')} to="/women">
         Women
       </NavLink>
-      <NavLink
-        onClick={onActivate}
-        title="Jewelry"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-        to="/jewelry"
-      >
+      <NavLink title="Jewelry" className={({ isActive }) => (isActive ? 'active' : '')} to="/jewelry">
         Jewelry
       </NavLink>
-      <NavLink
-        onClick={onActivate}
-        title="Electronics"
-        className={({ isActive }) => (isActive ? 'active' : '')}
-        to="/electronics"
-      >
+      <NavLink title="Electronics" className={({ isActive }) => (isActive ? 'active' : '')} to="/electronics">
         Electronics
       </NavLink>
     </StyledDropdown>
