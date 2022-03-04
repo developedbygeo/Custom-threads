8f01c429a6f69c8275ee60e5693012347766445c
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-05 01:26:45 +0200

    refactor: updating Home to use the useRetrieve hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,9 @@
-import { useSelector } from 'react-redux';
+import useRetrieve from '../hooks/useRetrieve';
 
 import Banner from '../components/UI/Banner';
-import { StyledSection } from '../components/UI/Container.styled';
-import { CtaButton } from '../components/UI/Button.styled';
-import { ItemCard, DescriptionCard } from '../components/UI/Card.styled';
 import { UtilityButton } from '../components/Extra/Controls';
 import { Filter } from '@styled-icons/fa-solid/Filter';
-import LoadingSpinner from '../components/UI/LoadingSpinner';
 import promoImage from '../assets/home.png';
-
-const gridSettings = {
-  cols: '1fr',
-  rows: 'auto',
-  gap: '3rem',
-};
 
 const memberPromoContent = {
   header: 'Member Exclusive',
@@ -23,25 +13,7 @@ const memberPromoContent = {
 };
 
 const Home = () => {
-  const items = useSelector((state) => state.product.products);
-
-  const fetchedItems = items.map((item) => {
-    return (
-      <ItemCard as="li" key={item.id} p="1rem">
-        <div>
-          <img src={item.image} alt={item.description} />
-        </div>
-        <DescriptionCard>
-          <h4>{item.title}</h4>
-          <p>${item.price}</p>
-          <CtaButton>Add to Cart</CtaButton>
-        </DescriptionCard>
-      </ItemCard>
-    );
-  });
-
-  const loading = <LoadingSpinner />;
-
+  const { status, ItemsComponent, LoadingSpinner } = useRetrieve();
   return (
     <>
       <Banner content={memberPromoContent} alt="models posing" />
@@ -49,9 +21,7 @@ const Home = () => {
         <Filter />
         Filter & Sort
       </UtilityButton>
-      <StyledSection className="products" as="ul" gridSettings={gridSettings}>
-        {items ? fetchedItems : loading}
-      </StyledSection>
+      {status ? <ItemsComponent /> : <LoadingSpinner />}
     </>
   );
 };
