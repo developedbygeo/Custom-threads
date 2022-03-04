ab19188460016d3ca46a92e1979616ba889c4b07
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-05 01:27:30 +0200

    style: updating spinner's message and message styling/colors

--- src/components/UI/LoadingSpinner.js
+++ src/components/UI/LoadingSpinner.js
@@ -14,24 +14,36 @@ const Spinner = styled.div`
     height: 40px;
     margin: 0.4rem;
     border-radius: 50%;
-    border: 6px solid ${({ theme }) => theme.colors.promoText};
-    border-color: ${({ theme }) => theme.colors.promoText} transparent
-      ${({ theme }) => theme.colors.promoText} transparent;
+    border: 6px solid ${({ theme }) => theme.colors.activeNav};
+    border-color: ${({ theme }) => theme.colors.activeNav} transparent
+      ${({ theme }) => theme.colors.activeNav} transparent;
     animation: ${spinning} 2s linear infinite;
   }
 `;
 const SpinnerMessage = styled.p`
   text-align: center;
+  font-weight: 600;
   letter-spacing: 0.11rem;
+  line-height: 3rem;
+  color: ${({ theme }) => theme.colors.activeNav};
+  & > span {
+    color: ${({ theme }) => theme.colors.mainText};
+  }
 `;
 Spinner.displayName = 'Spinner';
 
 const LoadingSpinner = () => {
   return (
-    <>
+    <div className="loading">
       <Spinner></Spinner>
-      <SpinnerMessage>Please try refreshing your browser</SpinnerMessage>
-    </>
+      <SpinnerMessage>
+        <span>
+          Content still loading...
+          <br />
+        </span>
+        Give it a second and try refreshing your browser!
+      </SpinnerMessage>
+    </div>
   );
 };
 
