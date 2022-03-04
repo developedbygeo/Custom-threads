f54f8496d5750de83ef0edd7ac50c1284cfbb30f
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-04 02:44:37 +0200

    refactor/fix: removed incorrect arrow fns and separated the new underline effect and colorInteract into two separate css snippets

--- src/components/shared/mixins.js
+++ src/components/shared/mixins.js
@@ -14,12 +14,37 @@ export const flexMixin = (justify, align, direction) => css`
   align-items: ${align};
 `;
 
-export const maxContainer = () => css`
+export const maxContainer = css`
   height: 100%;
   width: 100%;
 `;
 
-export const colorInteract = () => css`
+export const underlineEffect = css`
+  @media (hover: hover) {
+    &::after {
+      position: absolute;
+      content: '';
+      left: 5%;
+      right: 5%;
+      bottom: 1px;
+      width: 90%;
+      height: 0.75px;
+      transform-origin: right;
+      transform: scaleX(0);
+      background: ${({ theme }) => theme.colors.activeNav};
+      transition: transform 350ms ease;
+    }
+    &:hover {
+      &::after {
+        transform-origin: left;
+        transform: scaleX(1);
+      }
+    }
+  }
+`;
+
+export const colorInteract = css`
+  position: relative;
   transition: color 200ms ease-in-out;
   @media (hover: hover) {
     &:hover {
@@ -28,16 +53,20 @@ export const colorInteract = () => css`
   }
 `;
 
-export const showActive = () => css`
+export const showActive = css`
   color: ${({ theme }) => theme.colors.activeNav};
   font-weight: 600;
   position: relative;
   &::before {
     position: absolute;
+    bottom: 1px;
+    left: 5%;
+    right: 5%;
+    transform: translate(-50%, -50%);
     content: '';
-    bottom: -0.5rem;
-    width: 100%;
-    height: 0.15rem;
+    width: 90%;
+    transform: scaleX(1) !important;
+    height: 2px;
     background: ${({ theme }) => theme.colors.activeNav};
   }
 `;
