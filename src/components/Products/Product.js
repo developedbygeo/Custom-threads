711381454c9e9440b23dd7b0af1e5f699b231e35
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-08 00:53:14 +0200

    feat: adding different views based on whether Product is rendered as a cart item or a catalogue one

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,21 +1,51 @@
-import { ItemCard, DescriptionCard } from '../UI/Card.styled';
+import { ItemCard, DescriptionCard } from './Product.styled';
+import { Card } from '../UI/Card.styled';
 import { CtaButton } from '../UI/Button.styled';
+import { StyledUtilityBtn } from '../UI/Button.styled';
+import { BiPlus, BiMinus } from 'react-icons/bi';
 
-const Product = ({ product, onAddItem }) => {
+const Product = ({ product, isCartItem, onAddItem }) => {
+  const cartActions = isCartItem && (
+    <>
+      <Card className="cart-actions">
+        <StyledUtilityBtn>
+          <BiPlus />
+        </StyledUtilityBtn>
+        <StyledUtilityBtn>
+          <BiMinus />
+        </StyledUtilityBtn>
+      </Card>
+    </>
+  );
+
+  const productItemButton = onAddItem && (
+    <div className="cta-wrapper">
+      <CtaButton onClick={onAddItem}>Add to Cart</CtaButton>
+    </div>
+  );
+
+  const itemDescription = isCartItem ? (
+    <div className="cart-desc">
+      <p className="price">${product.price}</p>
+      <p>Quantity: {product.quantity}</p>
+    </div>
+  ) : (
+    <p className="price">${product.price}</p>
+  );
+
   return (
-    <ItemCard as="li" p="1rem">
+    <ItemCard as="li" p="1rem" isCartItem={isCartItem}>
       <div className="img-wrapper">
         <img src={product.image} alt={`${product.title} visual placeholder`} />
       </div>
       <DescriptionCard>
         <div className="info-wrapper">
           <h3>{product.title}</h3>
-          <p className="price">${product.price}</p>
+          {itemDescription}
         </div>
-        <div className="cta-wrapper">
-          <CtaButton onClick={onAddItem}>Add to Cart</CtaButton>
-        </div>
+        {productItemButton}
       </DescriptionCard>
+      {cartActions}
     </ItemCard>
   );
 };
