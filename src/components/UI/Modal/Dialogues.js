b719b854c8e627dd0912ab83f81f1446a1f465af
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-08 00:57:47 +0200

    feat; started working on the cart view

--- src/components/UI/Modal/Dialogues.js
+++ src/components/UI/Modal/Dialogues.js
@@ -1,3 +1,22 @@
-export const IntroDialogue = ({ onDisable, onOrder }) => {
-  return <></>;
+import { useSelector } from 'react-redux';
+import Product from '../../Products/Product';
+
+export const CartDialogue = ({ onDisable, onCheckout }) => {
+  const cartItems = useSelector((state) => state.cart.items);
+
+  const cartView = cartItems.map((item) => <Product key={item.id} product={item} isCartItem="true" />);
+
+  const emptyView = (
+    <>
+      <h4>No items found in your cart.</h4>
+      <p>Feel free to add some and check back again</p>
+    </>
+  );
+
+  return (
+    <>
+      <h3>My Cart</h3>
+      <div className="cart">{cartItems.length > 0 ? cartView : emptyView}</div>
+    </>
+  );
 };
