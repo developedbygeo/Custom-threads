27e3f0a9b1bd0e3aac258d015704c69ae9f70ec7
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-07 04:34:47 +0200

    feat: adding visual feedback to the CartButton when an item is added

--- src/components/Header/CartButton.js
+++ src/components/Header/CartButton.js
@@ -1,13 +1,31 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 import { StyledCartButton } from '../UI/Button.styled';
 import { Bag } from '@styled-icons/ionicons-outline/Bag';
 
 const CartButton = ({ onClick }) => {
+  const [isCartAnimated, setIsCartAnimated] = useState(false);
+  const itemsInCart = useSelector((state) => state.cart.totalQuantity);
+  const itemsExist = itemsInCart > 0;
+
+  useEffect(() => {
+    if (itemsInCart.length === 0) return;
+    setIsCartAnimated(true);
+    const animationTimer = setTimeout(() => {
+      setIsCartAnimated(false);
+    }, 400);
+    return () => clearTimeout(animationTimer);
+  }, [itemsInCart]);
+
+  const spanStyles = `total ${itemsExist ? 'total-active' : ''} ${isCartAnimated ? 'bump' : ''}`;
+
   return (
     <StyledCartButton onClick={onClick}>
       <span>
         <Bag className="bag" />
       </span>
-      <span className="total">0</span>
+      <span className={spanStyles}>{itemsInCart}</span>
     </StyledCartButton>
   );
 };
