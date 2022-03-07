9434772b63b929c7733cae26be3f4cb935080f21
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-07 04:31:57 +0200

    refactor: removing the fallback arg

--- src/components/Products/withSelectedProducts.js
+++ src/components/Products/withSelectedProducts.js
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 
 import LoadingSpinner from '../UI/LoadingSpinner';
 
-const withSelectedProducts = (Component, Fallback) => {
+const withSelectedProducts = (Component) => {
   const NewProductList = ({ retrieveID }) => {
     const [itemsExist, setItemsExist] = useState(false);
     const currentItems = useSelector((state) => state.data.products);
