9c2cb7a7449717ba50a4b4aa92785661dd89637c
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-07 04:33:10 +0200

    chore: importing the modularized nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,9 @@
-import { useState, useEffect } from 'react';
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-import Dropdown from './Dropdown.js';
 import StyledHeader from './Header.styled';
-import CartButton from './CartButton';
-import { Person } from '@styled-icons/bootstrap/Person';
-import { MenuAltRight } from '@styled-icons/boxicons-regular/MenuAltRight';
-import { Menu } from '@styled-icons/boxicons-regular/Menu';
+import Nav from './Nav.js';
 
 const Header = () => {
-  const [showDropdown, setShowDropdown] = useState(false);
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    setShowDropdown(false);
-  }, [pathname]);
-
-  const showDropdownHandler = () => setShowDropdown((prevState) => !prevState);
-
   return (
     <StyledHeader>
       <>
@@ -25,33 +11,7 @@ const Header = () => {
           <h1>Custom Threads</h1>
         </Link>
       </>
-      <nav className="mobileNav">
-        <button>
-          <Person />
-        </button>
-        <CartButton />
-        <div>
-          <button onClick={showDropdownHandler} className="burger" title="Toggle menu">
-            {showDropdown ? <MenuAltRight /> : <Menu />}
-          </button>
-          {showDropdown && <Dropdown onActivate={showDropdownHandler} />}
-        </div>
-      </nav>
-      <nav className="desktopNav">
-        <div>
-          <NavLink to="/home">Home</NavLink>
-          <NavLink to="/men">Men</NavLink>
-          <NavLink to="/women">Women</NavLink>
-          <NavLink to="/jewelry">Jewelry</NavLink>
-          <NavLink to="/electronics">Electronics</NavLink>
-        </div>
-        <div className="desktopNavCtrl">
-          <button>
-            <Person />
-          </button>
-          <CartButton />
-        </div>
-      </nav>
+      <Nav />
     </StyledHeader>
   );
 };
