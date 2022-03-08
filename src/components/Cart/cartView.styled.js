a8bc02ccf4d37bbe4dd8cd169f33e2164f9b2fb1
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-09 00:22:17 +0200

    style: adding a dedicated css snippet for the modal's cart view

---
+++ src/components/Cart/cartView.styled.js
@@ -0,0 +1,89 @@
+import { css } from 'styled-components';
+
+import { flexMixin, gridMixin } from '../shared/mixins';
+
+export const cartView = css`
+  max-height: 85vh;
+  min-height: 35vh;
+  height: auto;
+  ${flexMixin('space-evenly', 'flex-start', 'column')};
+
+  & > .cart-header {
+    padding-block: 1rem;
+    width: 100%;
+    h1 {
+      font-size: 2.8rem;
+    }
+    span {
+      color: ${({ theme }) => theme.colors.accent};
+    }
+  }
+  & > .cart {
+    ${gridMixin('1fr', 'auto', '2vh')};
+    max-height: 80vh;
+    overflow-y: auto;
+    & > li {
+      max-height: 15rem;
+    }
+    .description {
+      border: none;
+      border-left: 1px solid ${({ theme }) => theme.colors.cardBg};
+      padding: 1rem 2rem;
+    }
+    .img-wrapper {
+      padding-inline: 1rem;
+      img {
+        width: 80%;
+        max-width: 5rem;
+      }
+    }
+    .info-wrapper {
+      h3 {
+        text-align: left;
+        font-weight: 400;
+      }
+      .price,
+      .quantity {
+        font-weight: 500;
+      }
+    }
+  }
+  .cart-icon-left,
+  .cart-icon-right {
+    & > svg {
+      transform: translateY(2px) scale(1.15);
+    }
+  }
+  .cart-icon-left {
+    margin-right: 2vw;
+  }
+  .cart-icon-right {
+    margin-left: 2vw;
+  }
+  .cart-actions {
+    border-left: 1px solid ${({ theme }) => theme.colors.cardBg};
+    svg {
+      transform: scale(1.65);
+    }
+  }
+  .price-wrapper {
+    width: 100%;
+    padding: 2rem 0.5rem;
+    ${flexMixin('space-between', 'center', 'row')};
+    & > .price-header {
+      font-weight: 300;
+    }
+    & > .price-text {
+      color: ${({ theme }) => theme.colors.accent};
+      font-weight: 500;
+      letter-spacing: 1px;
+    }
+  }
+  .action-wrapper {
+    width: 100%;
+    ${flexMixin('space-between', 'center', 'row')};
+  }
+  .single-btn-layout {
+    justify-content: center;
+  }
+`;
