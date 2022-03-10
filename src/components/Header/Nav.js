0b02eb0b3c39b37204f88ff2e28cb5ad09f49671
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-10 05:20:06 +0200

    chore: importing CartView as a default import

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -4,7 +4,7 @@ import { NavLink, useLocation } from 'react-router-dom';
 
 import Dropdown from './Dropdown.js';
 import Modal from '../UI/Modal/Modal';
-import { CartView } from '../Cart/CartView.js';
+import CartView from '../Cart/CartView.js';
 import CartButton from './CartButton';
 import { Person } from '@styled-icons/bootstrap/Person';
 import { MenuAltRight } from '@styled-icons/boxicons-regular/MenuAltRight';
