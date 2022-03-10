0998eb39e2112e8e0c30dedde52696e270cc8a45
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-11 01:32:00 +0200

    style: decoupling the productDetails styling

---
+++ src/components/Products/ProductDetails.styled.js
@@ -0,0 +1,44 @@
+import { css } from 'styled-components';
+
+import { flexMixin } from '../shared/mixins';
+
+export const detailsStyles = css`
+  min-height: 65vh;
+  height: 100%;
+  margin: auto;
+  margin-top: 2vh;
+  padding: 2rem;
+  ${flexMixin('flex-start', 'center', 'column')};
+  & > .img-wrapper {
+    max-width: 20rem;
+    max-height: 20rem;
+    padding: 0;
+    margin: auto;
+  }
+`;
+
+export const detailsDescription = css`
+  height: auto;
+  justify-content: space-evenly;
+  text-align: left;
+  margin-block: 0;
+
+  .info-wrapper {
+    padding-inline: 2.5rem;
+    .prod-desc {
+      font-weight: 300;
+      &::first-letter {
+        text-transform: capitalize;
+      }
+    }
+  }
+
+  h2 {
+    margin-block: 4vh;
+    text-align: left;
+  }
+  .price {
+    text-align: right;
+    font-size: 2.1rem;
+  }
+`;
