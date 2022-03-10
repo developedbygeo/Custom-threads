bcdb06766ab34705bdc33ec92ceb699610de8784
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-10 02:18:51 +0200

    refactor: cleaned up unnecessary fallback

--- src/components/Products/withSelectedProducts.js
+++ src/components/Products/withSelectedProducts.js
@@ -1,24 +1,14 @@
-import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-
-import LoadingSpinner from '../UI/LoadingSpinner';
 
 const withSelectedProducts = (Component) => {
   const NewProductList = ({ retrieveID }) => {
-    const [itemsExist, setItemsExist] = useState(false);
     const currentItems = useSelector((state) => state.data.products);
-
-    useEffect(() => {
-      if (currentItems.length > 1) {
-        setItemsExist(true);
-      }
-    }, [currentItems]);
 
     const filteredItems = retrieveID
       ? currentItems.filter((item) => item.category === retrieveID)
       : currentItems;
 
-    return <>{itemsExist ? <Component products={filteredItems} /> : <LoadingSpinner />}</>;
+    return <Component products={filteredItems} />;
   };
   return NewProductList;
 };
