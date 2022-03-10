d5444d8024bf772fb272140a3dd199a8aa091967
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-11 01:13:55 +0200

    style: code cleanup and adding the essential helper fns to get the correct styling

--- src/components/Products/Product.styled.js
+++ src/components/Products/Product.styled.js
@@ -2,6 +2,8 @@ import styled, { css } from 'styled-components';
 
 import { Card } from '../UI/Card.styled';
 import { flexMixin } from '../shared/mixins';
+import { cartItem, cartItemDescription } from '../Cart/CartItem.styled';
+import { detailsStyles, detailsDescription } from './ProductDetails.styled';
 
 const ProductItem = css`
   height: 100%;
@@ -32,56 +34,6 @@ const ProductItem = css`
   }
 `;
 
-const CartItem = css`
-  height: 100%;
-  width: 100%;
-  margin: auto;
-  ${flexMixin('center', 'center', 'row')};
-  gap: 1rem;
-  border: 1px solid ${({ theme }) => theme.colors.cardBg};
-  & > .img-wrapper {
-    max-height: 15rem;
-    width: 100%;
-    padding: 0.5rem;
-  }
-
-  img {
-    height: 100%;
-    width: 100%;
-    display: block;
-  }
-  h3 {
-    text-align: center;
-    width: 41vw;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-  .price {
-    color: ${({ theme }) => theme.colors.accent};
-    font-size: 2rem;
-    font-weight: 600;
-    letter-spacing: 0.15rem;
-  }
-  .cart-actions {
-    padding: 0;
-  }
-  .cart-desc {
-    ${flexMixin('space-between', 'center', 'row')};
-  }
-`;
-
-const getItemStyle = ({ isCartItem }) => {
-  if (isCartItem) {
-    return CartItem;
-  }
-  return ProductItem;
-};
-
-export const ItemCard = styled(Card)`
-  ${getItemStyle}
-`;
-
 const ProductItemDescription = css`
   height: 100%;
   width: 100%;
@@ -97,6 +49,7 @@ const ProductItemDescription = css`
   }
   & > .cta-wrapper {
     padding: 2rem;
+    ${flexMixin('space-between', 'center', 'row-reverse')};
     button {
       min-height: 2rem;
       max-width: 25rem;
@@ -104,27 +57,37 @@ const ProductItemDescription = css`
   }
 `;
 
-const CartItemDescription = css`
-  height: 100%;
-  width: 100%;
-  border-top: none;
-  ${flexMixin('space-between', 'flex-start', 'column')};
-  gap: 0.5rem;
-  padding: 0;
-  line-height: 1.75;
-  text-align: center;
-  p {
-    letter-spacing: 0.1015rem;
-    font-weight: 500;
+const getItemStyle = ({ isCartItem }) => {
+  if (isCartItem) {
+    return cartItem;
   }
+  return ProductItem;
+};
+
+const getDetailsStyle = ({ isDetails }) => {
+  if (isDetails) {
+    console.log(detailsStyles);
+    return detailsStyles;
+  }
+};
+
+export const ItemCard = styled(Card)`
+  ${getItemStyle}
+  ${getDetailsStyle}
 `;
+
 const getDescriptionStyle = ({ isCartItem }) => {
   if (isCartItem) {
-    return CartItemDescription;
+    return cartItemDescription;
   }
   return ProductItemDescription;
 };
 
+const getDetailsDescription = ({ isDetails }) => {
+  if (isDetails) return detailsDescription;
+};
+
 export const DescriptionCard = styled(Card)`
   ${getDescriptionStyle}
+  ${getDetailsDescription}
 `;
