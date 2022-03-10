c8541311c77b08be93cbf63d90a0d89ca8500ba4
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-10 02:19:46 +0200

    feat: adding the standalone component for rendering cart items

---
+++ src/components/Cart/CartItem.js
@@ -0,0 +1,39 @@
+import { ItemCard, DescriptionCard } from '../Products/Product.styled';
+import { Card } from '../UI/Card.styled';
+import { StyledUtilityBtn } from '../UI/Button.styled';
+import { BiPlus, BiMinus } from 'react-icons/bi';
+
+const CartItem = ({ product, onAddItem, onRemoveItem }) => {
+  const cartActions = (
+    <>
+      <Card className="cart-actions">
+        <StyledUtilityBtn onClick={onAddItem} isCartAction customClr="white">
+          <BiPlus />
+        </StyledUtilityBtn>
+        <StyledUtilityBtn onClick={onRemoveItem} isCartAction customClr="white">
+          <BiMinus />
+        </StyledUtilityBtn>
+      </Card>
+    </>
+  );
+
+  return (
+    <ItemCard as="li" p="1rem" isCartItem>
+      <div className="img-wrapper">
+        <img src={product.image} alt={`${product.title} visual placeholder`} />
+      </div>
+      <DescriptionCard className="description">
+        <div className="info-wrapper">
+          <h3 title={product.title}>{product.title}</h3>
+          <div className="cart-desc">
+            <p className="price">${product.totalPrice.toFixed(2)}</p>
+            <p className="quantity">Quantity: {product.quantity}</p>
+          </div>
+        </div>
+      </DescriptionCard>
+      {cartActions}
+    </ItemCard>
+  );
+};
+
+export default CartItem;
