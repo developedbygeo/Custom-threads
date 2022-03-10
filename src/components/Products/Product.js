a3a868d58963b2e025d84b89569f2a3602d0516d
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-11 01:13:19 +0200

    refactor: adjust component to be rendered either as a list element or with a more detailed view

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,19 +1,47 @@
 import { ItemCard, DescriptionCard } from './Product.styled';
-import { CtaButton } from '../UI/Button.styled';
+import { CtaButton, StyledLink, StyledUtilityBtn } from '../UI/Button.styled';
+import { AiOutlineArrowLeft, AiOutlineEye } from 'react-icons/ai';
 
-const Product = ({ product, onAddItem }) => {
+const Product = ({ product, onAddItem, onGoBack, isDetails, asElement }) => {
+  const seeMoreDetailsButton = (
+    <StyledLink to={`/product-details/${product.id}`}>
+      <span>See more</span>
+      <span>
+        <AiOutlineEye />
+      </span>
+    </StyledLink>
+  );
+
+  const takeMeBackButton = (
+    <StyledUtilityBtn onClick={onGoBack} as="a" title="Go back">
+      <span className="icon">
+        <AiOutlineArrowLeft />
+      </span>
+      <span className="text">Take me back</span>
+    </StyledUtilityBtn>
+  );
+
   return (
-    <ItemCard as="li" p="1rem">
+    <ItemCard isDetails={isDetails} as={asElement || 'li'} p="1rem">
       <div className="img-wrapper">
         <img src={product.image} alt={`${product.title} visual placeholder`} />
       </div>
-      <DescriptionCard className="description">
+      <DescriptionCard isDetails={isDetails} className="description">
         <div className="info-wrapper">
-          <h3 title={product.title}>{product.title}</h3>
+          {isDetails ? (
+            <h2 title={product.title}>{product.title}</h2>
+          ) : (
+            <h3 title={product.title}>{product.title}</h3>
+          )}
+          {isDetails && <p className="prod-desc">{product.description}</p>}
           <p className="price">${product.price}</p>
         </div>
         <div className="cta-wrapper">
-          <CtaButton onClick={onAddItem}>Add to Cart</CtaButton>
+          <CtaButton onClick={onAddItem.bind(null, product)} title="Add this item to your cart">
+            Add to Cart
+          </CtaButton>
+          {!isDetails && seeMoreDetailsButton}
+          {isDetails && takeMeBackButton}
         </div>
       </DescriptionCard>
     </ItemCard>
