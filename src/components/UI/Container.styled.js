6f32d69875ac31a391ea4ed19571857f30b86768
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-12 00:10:08 +0200

    feat: adding the StyledCheckout component

--- src/components/UI/Container.styled.js
+++ src/components/UI/Container.styled.js
@@ -2,7 +2,8 @@ import styled, { css } from 'styled-components';
 
 import { devices } from '../shared/breakpoints';
 import { getLayout } from '../shared/utils';
-import { flexMixin } from '../shared/mixins';
+import { flexMixin, maxContainer } from '../shared/mixins';
+import { svgAdjustments, cartStyling, cartActions, priceStyling } from '../shared/styles/cartStyling';
 
 const sectionDefaults = css`
   width: 100vw;
@@ -14,6 +15,7 @@ export const StyledHero = styled.section`
   ${getLayout};
   background: ${({ bgClr }) => bgClr || 'inherit'};
   max-height: ${({ mHeight }) => mHeight || '45vh'};
+  height: ${({ h }) => h || 'initial'};
 
   & > div {
     max-height: 45vh;
@@ -29,6 +31,13 @@ export const StyledHero = styled.section`
   }
   h2 {
     margin-block: 0.5rem;
+  }
+
+  .notFound img {
+    ${maxContainer}
+  }
+  .notFound button {
+    font-size: 2.4rem;
   }
 `;
 
@@ -100,4 +109,80 @@ export const StyledSection = styled.section`
   margin: auto;
   margin-top: 2vh;
   ${getLayout}
+`;
+
+// TODO StyledCheckout tablet landscape width 70%;
+export const StyledCheckout = styled(StyledSection)`
+  ${svgAdjustments};
+  ${cartStyling};
+  ${cartActions};
+  ${priceStyling};
+  min-height: 50vh;
+  margin-top: 1vh;
+  justify-content: space-between;
+  height: 97.5%;
+  .cart {
+    height: 95%;
+    width: 100%;
+    max-height: 72.5vh;
+    margin-block: 2vh;
+  }
+  & > .img-wrapper {
+    max-width: 15rem;
+    max-width: 15rem;
+    padding: 0;
+    img {
+      padding: 1rem;
+      max-height: 10rem;
+      max-width: 25rem;
+    }
+  }
+  .cart-actions {
+    width: 70%;
+  }
+  & > .price-wrapper {
+    width: 100%;
+    padding: 1rem 0.5rem;
+    margin-bottom: 1vh;
+  }
+  .action-wrapper {
+    ${flexMixin('space-between', 'center', 'row')};
+    width: 100%;
+    .return-btn {
+      border: 1px solid ${({ theme }) => theme.colors.heroBg};
+      transition: all 50ms ease-in-out;
+      @media (hover: hover) {
+        &:hover {
+          border-color: ${({ theme }) => theme.colors.ctaSecondaryBgClr};
+        }
+      }
+    }
+  }
+  .single-btn-layout {
+    justify-content: center;
+  }
+  & > p {
+    width: 100%;
+    padding: 0.75rem;
+    .step {
+      background: rgba(${({ theme }) => theme.colors.activeNavRGB}, 0.8);
+      color: ${({ theme }) => theme.colors.mainBg};
+      font-weight: 700;
+      padding: 0.5rem 1rem;
+      border-radius: 50%;
+      margin-right: 2vw;
+    }
+    .step,
+    .step-description {
+      display: inline-block;
+    }
+    .step-description {
+      font-weight: 300;
+      letter-spacing: 0.125rem;
+      font-size: 1.9rem;
+    }
+    .step-description::first-letter {
+      text-transform: capitalize;
+    }
+  }
 `;
