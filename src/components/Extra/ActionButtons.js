826f8bee58545b927880bc9711c77bcd213cdf05
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-12 00:13:53 +0200

    refactor: adding ActionButtons for cart & checkout

---
+++ src/components/Extra/ActionButtons.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { CtaButton, SecondaryButton } from '../UI/Button.styled';
+import { AiOutlineShoppingCart, AiOutlineArrowLeft } from 'react-icons/ai';
+
+const ActionButtons = ({ isCart, isCheckout, onDisable, onAction }) => {
+  const cartItems = useSelector((state) => state.cart.items);
+  const itemsExist = cartItems.length > 0;
+
+  const cartView = (
+    <div className={`action-wrapper ${!itemsExist ? 'single-btn-layout' : ''}`}>
+      <SecondaryButton onClick={onDisable} className="return-btn">
+        <span className="cart-icon-left">
+          <AiOutlineArrowLeft />
+        </span>
+        <span>Return</span>
+      </SecondaryButton>
+      {itemsExist && (
+        <Link to="/checkout" onClick={onDisable}>
+          <CtaButton ctaAltHover disableTransform className="checkout-btn">
+            <span>Checkout</span>
+            <span className="cart-icon-right">
+              <AiOutlineShoppingCart />
+            </span>
+          </CtaButton>
+        </Link>
+      )}
+    </div>
+  );
+
+  const checkoutView = (
+    <div className={`action-wrapper ${!itemsExist ? 'single-btn-layout' : ''}`}>
+      <SecondaryButton onClick={onDisable} className="return-btn">
+        <span className="cart-icon-left">
+          <AiOutlineArrowLeft />
+        </span>
+        <span>Return</span>
+      </SecondaryButton>
+      {itemsExist && (
+        <CtaButton onClick={onAction} ctaAltHover disableTransform className="checkout-btn">
+          <span>Continue</span>
+          <span className="cart-icon-right">
+            <AiOutlineShoppingCart />
+          </span>
+        </CtaButton>
+      )}
+    </div>
+  );
+
+  const genericView = (
+    <div className="action-wrapper">
+      <SecondaryButton onClick={onDisable} className="return-btn">
+        <span className="cart-icon-left">
+          <AiOutlineArrowLeft />
+        </span>
+        <span>Return</span>
+      </SecondaryButton>
+      {itemsExist && (
+        <CtaButton onClick={onAction} ctaAltHover disableTransform className="checkout-btn">
+          <span>Continue</span>
+          <span className="cart-icon-right">
+            <AiOutlineShoppingCart />
+          </span>
+        </CtaButton>
+      )}
+    </div>
+  );
+  return isCart ? cartView : isCheckout ? checkoutView : genericView;
+};
+
+export default React.memo(ActionButtons);
