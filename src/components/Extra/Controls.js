98ee536f99931a1e20f6511df74b575dca2798e2
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-13 01:46:37 +0200

    feat: adding the sort button

--- src/components/Extra/Controls.js
+++ src/components/Extra/Controls.js
@@ -3,21 +3,34 @@ import { useState } from 'react';
 import Drawer from '../UI/Drawer/Drawer';
 import Filters from './Filters';
 import { StyledUtilityBtn } from '../UI/Button.styled';
-import { Card } from '../UI/Card.styled';
+import { Card, ActionWrapper } from '../UI/Card.styled';
+import { Filter } from '@styled-icons/fa-solid/Filter';
+import { CaretUpFill } from '@styled-icons/bootstrap/CaretUpFill';
+import { CaretDownFill } from '@styled-icons/bootstrap/CaretDownFill';
 
-export const UtilityButton = ({ children }) => {
+const wrapperSettings = {
+  justify: 'space-between',
+  align: 'center',
+  dir: 'row',
+};
+
+export const Controls = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const menuToggleHandler = () => {
     setMenuOpen((prevState) => !prevState);
   };
 
+  // TODO finish the sorting logic - maybe implement sort on other pages as well
+
   return (
-    <>
-      <Card sticky p="1rem">
+    <ActionWrapper sticky flexSettings={wrapperSettings}>
+      <Card p="1rem" customMargin="0%">
         <StyledUtilityBtn onClick={menuToggleHandler}>
-          <span className="icon">{children[0]}</span>
-          <span className="text">{children[1]}</span>
+          <span className="icon">
+            <Filter />
+          </span>
+          <span className="text">Filter</span>
         </StyledUtilityBtn>
       </Card>
       {menuOpen && (
@@ -25,6 +38,15 @@ export const UtilityButton = ({ children }) => {
           <Filters />
         </Drawer>
       )}
-    </>
+      <Card sticky p="1rem" customMargin="0%">
+        <StyledUtilityBtn>
+          <span className="text">Sort</span>
+          <span className="icon smallIcon">
+            <CaretUpFill />
+            {/* <CaretDownFill /> */}
+          </span>
+        </StyledUtilityBtn>
+      </Card>
+    </ActionWrapper>
   );
 };
