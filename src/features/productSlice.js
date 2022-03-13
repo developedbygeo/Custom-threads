0361be5a247249b571256564fabafc17bcfb58fa
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-13 02:05:06 +0200

    feat: adding the filtering logic

--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -2,11 +2,22 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const productSlice = createSlice({
   name: 'products',
-  initialState: { fetched: false, products: [] },
+  initialState: { fetched: false, products: [], filtered: [] },
   reducers: {
     addProducts(state, action) {
       state.products = action.payload;
+      state.filtered = action.payload;
       state.fetched = true;
+    },
+    filterByCategory(state, action) {
+      state.filtered = state.products.filter((item) => {
+        return action.payload.filters.some((activeFilter) => {
+          return item.category === activeFilter && item.price <= action.payload.price;
+        });
+      });
+    },
+    resetFilters(state) {
+      state.filtered = state.products;
     },
   },
 });
