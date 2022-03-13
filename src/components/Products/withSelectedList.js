15d71ecf10daa154a7a0a9b8e24fa4a3a25ad066
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-13 02:07:51 +0200

    refactor: HOC will now render the correct items

--- src/components/Products/withSelectedList.js
+++ src/components/Products/withSelectedList.js
@@ -3,10 +3,9 @@ import { useSelector } from 'react-redux';
 const withSelectedList = (Component) => {
   const NewProductList = ({ retrieveID }) => {
     const currentItems = useSelector((state) => state.data.products);
+    const filtered = useSelector((state) => state.data.filtered);
 
-    const filteredItems = retrieveID
-      ? currentItems.filter((item) => item.category === retrieveID)
-      : currentItems;
+    const filteredItems = retrieveID ? currentItems.filter((item) => item.category === retrieveID) : filtered;
 
     return <Component products={filteredItems} />;
   };
