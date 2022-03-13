531f7818c846b0f6d3cf8f5c06aee3c0a2b30941
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-13 02:13:43 +0200

    feat: adding the form logic for filtering

--- src/components/Extra/Filters.js
+++ src/components/Extra/Filters.js
@@ -1,12 +1,21 @@
-import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { productActions } from '../../features/productSlice';
+import { useState, useRef } from 'react';
 
 import StyledFilters from './Filters.styled';
 import { CtaButton } from '../UI/Button.styled';
 import Checkbox from '../UI/Checkbox/Checkbox';
 import CustomSlider from '../UI/Slider.styled';
 
+import { getFormData } from '../shared/utils';
+
 const Filters = () => {
+  const dispatch = useDispatch();
   const [price, setPrice] = useState(1000);
+  const menRef = useRef();
+  const womenRef = useRef();
+  const jewelryRef = useRef();
+  const electroRef = useRef();
 
   const changePriceHandler = (e) => {
     setPrice(e.target.value);
@@ -14,16 +23,23 @@ const Filters = () => {
 
   const submitFiltersHandler = (e) => {
     e.preventDefault();
+    const filters = getFormData(
+      { status: menRef.current.isChecked(), id: "men's clothing" },
+      { status: womenRef.current.isChecked(), id: "women's clothing" },
+      { status: jewelryRef.current.isChecked(), id: 'jewelery' },
+      { status: electroRef.current.isChecked(), id: 'electronics' }
+    );
+    dispatch(productActions.filterByCategory({ filters, price }));
   };
 
   return (
     <>
       <StyledFilters onSubmit={submitFiltersHandler}>
         <h3>By Category</h3>
-        <Checkbox>Men</Checkbox>
-        <Checkbox>Women</Checkbox>
-        <Checkbox>Jewelry</Checkbox>
-        <Checkbox>Electronics</Checkbox>
+        <Checkbox ref={menRef}>Men</Checkbox>
+        <Checkbox ref={womenRef}>Women</Checkbox>
+        <Checkbox ref={jewelryRef}>Jewelry</Checkbox>
+        <Checkbox ref={electroRef}>Electronics</Checkbox>
         <h3>By Price</h3>
         <div className="price">
           <label>Price:</label>
