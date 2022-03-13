c9debb2dfd32054aa987842ed59156f4b550a0c9
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-13 02:05:21 +0200

    style: updating the aside's position

--- src/components/UI/Drawer/Drawer.styled.js
+++ src/components/UI/Drawer/Drawer.styled.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const StyledDrawer = styled.aside`
-  position: sticky;
-  top: 7%;
+  position: absolute;
+  top: 80%;
   z-index: 10;
   max-width: 40vw;
   height: auto;
