caff07b5b2565239a0554af2d1d543bb0e1f93a8
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-13 02:07:01 +0200

    style: adding small svg snippet

--- src/components/shared/styles/miscStyling.js
+++ src/components/shared/styles/miscStyling.js
@@ -4,3 +4,10 @@ export const labelStyle = css`
   font-weight: 300;
   font-size: 2rem;
 `;
+
+export const smallIconStyle = css`
+  .smallIcon > svg {
+    max-width: 2.5rem;
+    width: 2.5rem;
+  }
+`;
