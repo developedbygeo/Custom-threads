2fb0011158c073b8a5c6d662b6c959808c2d9725
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-14 04:18:05 +0200

    refactor: code cleanup and adding outlet

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,38 +1,21 @@
-import { useCallback } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import useCheckout from '../hooks/useCheckout';
+import { Outlet } from 'react-router-dom';
 
-import CartView from '../components/Cart/CartView';
 import ActionButtons from '../components/Extra/ActionButtons';
 import { StyledCheckout } from '../components/UI/Container.styled';
 
-const steps = ['checkout', 'details', 'success'];
-// steps[steps.indexOf(pathname.replace('/', ''))+1]
-// TODO to get to the next one dynamically
-
-const layoutSettings = {
-  justify: 'center',
-  align: 'center',
-  dir: 'column',
-};
-
 const Checkout = () => {
-  const navigate = useNavigate();
-  const { pathname } = useLocation();
-  const currentStep = steps.indexOf(pathname.replace('/', ''));
-
-  const onReturnHandler = useCallback(() => {
-    navigate(-1);
-  }, [navigate]);
+  const { onReturn, onProceed, layout, currentStepNum, currentStepText } = useCheckout();
 
   return (
     <>
-      <StyledCheckout flexSettings={layoutSettings}>
+      <StyledCheckout flexSettings={layout}>
         <p className="step-indicator">
-          <span className="step">{currentStep}</span>
-          <span className="step-description">{steps[currentStep]}</span>
+          <span className="step">{currentStepNum + 1}</span>
+          <span className="step-description">{currentStepText}</span>
         </p>
-        <CartView />
-        <ActionButtons isCheckout onDisable={onReturnHandler} />
+        <Outlet />
+        <ActionButtons isCheckout onDisable={onReturn} onAction={onProceed} />
       </StyledCheckout>
     </>
   );
