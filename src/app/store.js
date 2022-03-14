986bd1a43728211a35aecdbf62ba20d834af82f3
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-15 00:16:15 +0200

    chore: updating to include the auth reducer

--- src/app/store.js
+++ src/app/store.js
@@ -3,7 +3,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartSlice from '../features/cartSlice';
 import productSlice from '../features/productSlice';
 import uiSlice from '../features/uiSlice';
+import authSlice from '../features/authSlice';
 
 export const store = configureStore({
-  reducer: { cart: cartSlice.reducer, data: productSlice.reducer, ui: uiSlice.reducer },
+  reducer: {
+    cart: cartSlice.reducer,
+    data: productSlice.reducer,
+    ui: uiSlice.reducer,
+    auth: authSlice.reducer,
+  },
 });
