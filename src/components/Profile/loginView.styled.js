4bb545458c89dca1829ca1d9dd72cc64ef86a062
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-21 03:27:06 +0200

    style: adding the login view-specific snippet

---
+++ src/components/Profile/loginView.styled.js
@@ -0,0 +1,21 @@
+import { css } from 'styled-components';
+
+import { devices } from '../shared/breakpoints';
+import loginBlob from '../../assets/blobs/loginblob.png';
+
+export const loginView = css`
+  .login-blob {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    left: 0;
+    bottom: 0%;
+    background: url(${loginBlob}) center no-repeat;
+    background-size: cover;
+    pointer-events: none;
+    opacity: 0.35;
+  }
+  @media ${devices.laptop} {
+    width: 45vw;
+  }
+`;
