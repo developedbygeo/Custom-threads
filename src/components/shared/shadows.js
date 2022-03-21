b53ab8115b9c51402b13bf435c420e990c70e724
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-21 03:28:33 +0200

    feat: adding elevation shadow snippets

---
+++ src/components/shared/shadows.js
@@ -0,0 +1,8 @@
+import { css } from 'styled-components';
+
+export const boxElevationLow = css`
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.05), 0 3px 6px rgba(0, 0, 0, 0.15);
+`;
+export const boxElevationMid = css`
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.06), 0 3px 6px rgba(0, 0, 0, 0.1);
+`;
