5592f68d1e3f73d6aa0f805a144809b879c98765
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-22 03:18:06 +0200

    style: adding button elevation snippet

--- src/components/shared/shadows.js
+++ src/components/shared/shadows.js
@@ -6,3 +6,7 @@ export const boxElevationLow = css`
 export const boxElevationMid = css`
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.06), 0 3px 6px rgba(0, 0, 0, 0.1);
 `;
+
+export const buttonElevationMid = css`
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+`;
