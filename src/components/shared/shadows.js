3952fcd401d4c2cfe8feaf855250fe8c6b004d7e
georgiosan <georgiosanagnostou@hotmail.com>
2022-03-23 02:56:26 +0200

    style: adding buttonElevationLow snippet

--- src/components/shared/shadows.js
+++ src/components/shared/shadows.js
@@ -7,6 +7,10 @@ export const boxElevationMid = css`
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.06), 0 3px 6px rgba(0, 0, 0, 0.1);
 `;
 
+export const buttonElevationLow = css`
+  box-shadow: 0 3.2px 7.2px 0 rgb(0 0 0 / 7.5%), 0 0.6px 1.8px 0 rgb(0 0 0 / 5.5%);
+`;
+
 export const buttonElevationMid = css`
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
 `;
